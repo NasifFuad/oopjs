0738ae22eb9caecafb818a4706257362c1d93683
NasifFuad <nasiffuad65@gmail.com>
2021-07-05 11:54:40 +0600

    1 file modified, 2 added, 2 deleted

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Import stylesheets
 import './style.css';
 import axios from "axios"
-import FactoryPatternClass from "./FactoryDesignPattern.js"
-import ConstructorPatternClass from "./constructorDesignPattern.js"
+import FactoryPatternClass from "./Design_Patterns_OOP/FactoryDesignPattern.js"
+import ConstructorPatternClass from "./Design_Patterns_OOP /constructorDesignPattern.js"
 
 
 const factoryPatternDemo = new FactoryPatternClass(1,3)
